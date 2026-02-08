62e3d3dcdfeb501a1ca801664db19aa9b3b94a02
이상선 <sunyi233@gmail.com>
2026-02-08 12:34:43 +0900

    Modify GetBackendFunctionResult to update Endpoint and handle token
    Updated the Endpoint URL and added handling for 'GetAccessToken' case.

--- _JS/GetBackendFunctionResult.js
+++ _JS/GetBackendFunctionResult.js
@@ -1,7 +1,7 @@
 async function GetBackendFunctionResult(Name, Body)
 {
     // set Endpoint
-    const Endpoint = '................';
+    const Endpoint = 'https://aev92uqq1h.execute-api.ap-northeast-2.amazonaws.com/';
 
     // set Options
     let Options = {method:'POST'};
@@ -10,6 +10,9 @@ async function GetBackendFunctionResult(Name, Body)
     // call
     const Result = await fetch(Endpoint + Name, Options).catch((Error) => {return null;});
     if (Result.ok == false) return null;
+
+    // GetAccessToken case: access token never handled out of this function
+    if (Name == 'GetAccessToken') {localStorage.setItem('AccessToken', (await Result.text()));}
 
     return Result;
 }
